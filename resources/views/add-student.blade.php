adc11d78c79aa6cc12aaf5fd04d9d3db0f204514
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-13 15:42:17 +0300

    Toastr and Sweet Alert

--- resources/views/add-student.blade.php
+++ resources/views/add-student.blade.php
@@ -8,6 +8,9 @@
     <title>Add Student</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+        integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
+        crossorigin="anonymous" />
 </head>
 
 <body>
@@ -35,7 +38,7 @@
                                     <label for="file">Choose Profile Image</label>
                                     <input type="file" class="form-control" id="file" name="file"
                                         onchange="previewFile(this)">
-                                    <img id="previewImg"  style="max-width: 130px; margin-top: 20px;">
+                                    <img id="previewImg" style="max-width: 130px; margin-top: 20px;">
                                 </div>
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </form>
@@ -45,16 +48,21 @@
             </div>
         </div>
     </section>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+        integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+        crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
         integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
         integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
     </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+        integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"
+        integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA=="
+        crossorigin="anonymous"></script>
     <script>
         function previewFile(input){
             var file = $("input[type=file]").get(0).files[0];
@@ -67,6 +75,18 @@
             }
         }
     </script>
+    @if (Session::has('student_added'))
+    <script>
+        toastr.success("{!! Session::get('student_added') !!}")
+    </script>
+    @endif
+    @if (Session::has('student_added'))
+    <script>
+        swal("Great Job!", "{!! Session::get('student_added') !!}", "success", {
+            button: "OK"
+        });
+    </script>
+    @endif
 </body>
 
 </html>
