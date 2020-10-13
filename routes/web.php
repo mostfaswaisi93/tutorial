b8a980ca9fada9dd7aa1032ace0e6bd4be160009
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-13 17:15:22 +0300

    Helpers Functions

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\EditorController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -23,3 +24,6 @@ Route::get('/delete-student/{id}', [StudentController::class, 'deleteStudent']);
 // Contact Form
 Route::get('/contact-us', [ContactController::class, 'contact']);
 Route::post('/send-message', [ContactController::class, 'sendEmail'])->name('contact.send');
+
+// Helpers
+Route::get('/get-name', [TestController::class, 'getFirstLastName']);
