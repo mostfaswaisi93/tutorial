d622a7b67e9a6b2f451d46fe05922b93e2a72abd
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-13 14:12:03 +0300

    TinyMCE WYSIWYG HTML Editor

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\EditorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// TinyMCE WYSIWYG HTML Editor
+Route::get('/editor', [EditorController::class, 'editor']);
