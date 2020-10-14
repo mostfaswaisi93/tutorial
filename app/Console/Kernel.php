9d16e12e4430e095f9a9b0c7120313fb1d4b1291
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-14 18:39:16 +0300

    Upgrade - Livewire Installation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Users::class
+        //
     ];
 
     /**
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Accessors And Mutators
-Route::get('/add-student', [StudentController::class, 'addStudent']);
-Route::get('/students', [StudentController::class, 'getStudents']);
