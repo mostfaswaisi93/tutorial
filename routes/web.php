f395b7104b19af20a19ec5319c8fd489e5324325
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-15 03:22:54 +0300

    Livewire Pagination

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\User;
+use App\Http\Livewire\Users;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,6 @@ Route::get('/', function () {
 
 // Livewire With Database
 Route::get('/user', User::class);
+
+// Livewire Pagination
+Route::get('/users', Users::class);
