6a4f28cfeaa22be12f2dac130d112a4143d2a39d
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-22 04:39:02 +0300

    count and crossJoin

--- app/ExampleCollection.php
+++ app/ExampleCollection.php
@@ -6,12 +6,24 @@ class ExampleCollection
 {
     public function example()
     {
-        return collect([1, 2, 3, 4, 5])
-            ->contains(function ($value, $key) {
-                return $value > 4;
-            }); // contains
+        $data = [
+            1,
+            2 => [5, 6],
+            3,
+            4,
+            'seven'
+        ];
 
-        return collect(["  0015"])
-            ->containsStrict(' 15')->first(); // containsStrict
+        return collect($data)->count(); // count
+
+        // $collections = collect([1, 2]);
+        $collections = collect(['Mustang', 'GT', 'F150']);
+        // return $collections->crossJoin(['a', 'b'], ['c', 'd']);
+        return $collections->crossJoin([
+            ['automatic', 'manual'],
+            ['blue', 'black', 'white', 'yellow', 'gray'],
+            [2018, 2019, 2020]
+        ]); // crossJoin
+
     }
 }
