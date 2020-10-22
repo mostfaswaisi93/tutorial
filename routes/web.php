7c355261a3f4113cb0e043feefd63ec348ee33a9
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-22 03:04:14 +0300

    Part 1

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,24 @@
 
 use App\Http\Controllers\CrudController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionnaireController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
 Route::get('/youtube', [CrudController::class, 'getVideo']);
 Route::get('/customers', [CustomerController::class, 'list']);
+
+// Questionnaire Survey App
+Route::get('/questionnaire', [QuestionnaireController::class, 'index']);
+
+Route::get('/questionnaires/create', [QuestionnaireController::class, 'create']);
+Route::post('/questionnaires', [QuestionnaireController::class, 'store']);
+Route::get('/questionnaires/{questionnaire}', [QuestionnaireController::class, 'show']);
