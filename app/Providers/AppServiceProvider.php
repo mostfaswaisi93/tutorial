9aa1838cc109316f2370a22d17c8d5b25e6407eb
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-22 15:33:28 +0300

    Upgrade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Billing\PaymentGateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(PaymentGateway::class, function ($app) {
-            return new PaymentGateway('usd');
-        });
+        //
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-use App\Http\Controllers\CrudController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\PayOrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/youtube', [CrudController::class, 'getVideo']);
-Route::get('/customers', [CustomerController::class, 'list']);
-
-// Service Container
-Route::get('/pay', [PayOrderController::class, 'store']);
