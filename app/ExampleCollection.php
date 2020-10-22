fe534bba7966b668f40a405f4be758e63709d111
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-22 04:15:42 +0300

    collapse

--- app/ExampleCollection.php
+++ app/ExampleCollection.php
@@ -6,36 +6,12 @@ class ExampleCollection
 {
     public function example()
     {
-        // $data = [10, 20, 30];
-
         $data = [
-            ['price' => 10000, 'tax' => 500, 'active' => true],
-            ['price' => 20000, 'tax' => 700, 'active' => false],
-            ['price' => 30000, 'tax' => 900, 'active' => true]
+            [0 => ['array1']],
+            [1 => ['array2']],
+            [3, 4, 5]
         ];
 
-        // return collect($data)->average('price'); 
-        return collect($data)->average(function ($value) { // average
-            if (!$value['active']) {
-                return null;
-            }
-            return $value['price'] + $value['tax'];
-        });
-
-        return collect($data)->max(function ($value) { // max
-            if (!$value['active']) {
-                return null;
-            }
-            return $value['price'] + $value['tax'];
-        });
-
-        return collect($data)->median('price'); // median  
-
-        return collect($data)->min(function ($value) { // min
-            if (!$value['active']) {
-                return null;
-            }
-            return $value['price'] + $value['tax'];
-        });
+        return collect($data)->collapse(); // collapse
     }
 }
