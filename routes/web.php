fd49751177a6a85ebacc18ef2c6395f13e383ec6
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-11-03 16:07:30 +0200

    Upgrade

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,7 @@
 <?php
 
-use App\Http\Livewire\Images;
-use App\Http\Livewire\Students;
-use App\Http\Livewire\Uploads;
-use App\Http\Livewire\User;
-use App\Http\Livewire\Users;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Livewire With Database
-Route::get('/user', User::class);
-
-// Livewire Pagination
-Route::get('/users', Users::class);
-
-// Livewire CRUD
-Route::get('/students', Students::class);
-
-// Livewire File Upload 
-Route::get('/uploads', Uploads::class);
-
-// Livewire Multiple Image Upload
-Route::get('/upload-images', Images::class);
