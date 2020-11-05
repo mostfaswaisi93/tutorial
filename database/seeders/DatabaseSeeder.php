3f94a23d7df718e50fd7efc044ab02df6c225f66
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-11-06 00:08:57 +0200

    Upgrade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
     public function run()
     {
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(TagsTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,7 @@
 <?php
 
-use App\Http\Controllers\EcommerceController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::any('/ecommerce', [EcommerceController::class, 'index'])->name('ecommerce.index');
-Route::any('/ecommerce/list', [EcommerceController::class, 'index_list'])->name('ecommerce.index_list');
-Route::get('/ecommerce/create', [EcommerceController::class, 'create'])->name('ecommerce.create');
-Route::post('/ecommerce/create', [EcommerceController::class, 'store'])->name('ecommerce.store');
-Route::get('/ecommerce/{id}/edit', [EcommerceController::class, 'edit'])->name('ecommerce.edit');
-Route::patch('/ecommerce/{id}/edit', [EcommerceController::class, 'update'])->name('ecommerce.update');
-Route::delete('/ecommerce/{id}', [EcommerceController::class, 'destroy'])->name('ecommerce.destroy');
-
-Auth::routes();
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
