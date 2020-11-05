360196fb9b9ed8b5b3130328d3a1f1fb3bec819a
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-11-06 00:06:16 +0200

    Search Filter

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Tag;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Factory::create();
+
+        $categories = Category::all();
+
+        foreach ($categories as $category) {
+
+            for ($i = 1; $i <= 30; $i++) {
+
+                $product = Product::create([
+                    'name'          => $category->name . ' ' . $i,
+                    'description'   => $faker->paragraph(),
+                    'price'         => $faker->randomFloat(2, 100, 5000),
+                    'image'         => 'https://via.placeholder.com/150?text=' . str_replace(' ', '+', $category->name) . '+' . $i,
+                    'category_id'   => $category->id,
+                ]);
+
+                $tags = Tag::inRandomOrder()->take(3)->pluck('id')->toArray();
+
+                $product->tags()->attach($tags);
+            }
+        }
+    }
+}
