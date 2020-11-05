6744ea2dda1e60f0efaac428346494d260241e80
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-11-05 02:53:32 +0200

    Upgrade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PlacesSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
